ae4f2129ea13f3af3440f9c5562d6cf72cd0b815
shimokariya yuki <shimokari0321@gmail.com>
2020-09-01 18:23:22 +0900

    userテーブルの作成とFactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confimation {password}
-    first-name {Faker::Name.first_name}
-    family-name {Faker::Name.family_name}
-    first-name-kana {Faker::Name.first_name_kana}
-    family-name-kana {Faker::Name.family_name_kana}
+    first_name {Faker::Name.first_name}
+    family_name {Faker::Name.family_name}
+    first_name_kana {Faker::Name.first_name_kana}
+    family_name_kana {Faker::Name.family_name_kana}
     birthday {Faker::Date.birthday}
   end
 end
