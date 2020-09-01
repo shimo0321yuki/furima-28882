dfce8bc4137de289af66646cfb976630a66ff3cd
shimokariya yuki <shimokari0321@gmail.com>
2020-09-01 19:35:13 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :basic_auth
   # before_filter :authenticate_user! #, :except=>[:hello]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, kyes: [:nickname])
+  end
 
 
   private
