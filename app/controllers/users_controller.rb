b5e5ec89dc062fddaf42cb94c6a7291e14231ec1
shimokariya yuki <shimokari0321@gmail.com>
2020-09-01 21:28:27 +0900

    ルーティングとコントローラーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    current_user.update(user_rparams)
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:nickname, :email)
+  end
+
 end
