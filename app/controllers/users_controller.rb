4f36afa6b4f5fcbac9123b32017cc456346fcc50
shimokariya yuki <shimokari0321@gmail.com>
2020-09-04 17:14:38 +0900

    バリデーションの設定、エラーメッセージの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = User.new(user_params)
+    #バリデーションで問題があれば、保存されず「新規登録画面」に戻る
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render 'new'
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users 
+  resources :users, only: [:edit, :update] 
   resources :items, only: [:index, :search, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-#only: [:edit, :update]
