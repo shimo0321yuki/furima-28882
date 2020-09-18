f542b997ea98deb89d3f720e348e1d2867606ead
shimokariya yuki <shimokari0321@gmail.com>
2020-09-18 13:56:22 +0900

    basic認証の環境変数

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  # before_action :authenticate_user!
-  # before_filter :authenticate_user! #, :except=>[:hello]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -15,12 +13,8 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username ==  ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_ATH_PASSWORD"]
     end
   end
 end
 
-# ENV["BASIC_AUTH_USER"]
-# ENV["BASIC_ATH_PASSWORD"]
-
-# :email, :password,
